d2c2952b63f876e6c43e3e709420983df4195a8b
The_Tamil_Developer <36486498+Nataraj2609@users.noreply.github.com>
2020-08-14 21:58:31 +0530

    Added dependency to spring boot application commit

---
+++ springboot-crud-backend/src/main/java/com/nataraj/product1/springbootcrudbackend/SpringbootCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nataraj.product1.springbootcrudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudBackendApplication.class, args);
+	}
+
+}
---
+++ springboot-crud-backend/src/test/java/com/nataraj/product1/springbootcrudbackend/SpringbootCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nataraj.product1.springbootcrudbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
